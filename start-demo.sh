b247fbaa5a29eeb751aed8818511e7d6e0a32bd2
Emmanuel Nyachoke <achachiez@gmail.com>
2022-01-26 11:21:01 +0300

    Update scripts with shebang and clean up the exports

--- start-demo.sh
+++ start-demo.sh
@@ -1,19 +1,20 @@
+#!/bin/bash
 export OZONE_DIR=$PWD/ozone && \
 mkdir -p $OZONE_DIR
-# Download the project && \
+# Download the project
 export VERSION=1.0.0-SNAPSHOT && \
 ./mvnw org.apache.maven.plugins:maven-dependency-plugin:3.2.0:get -DremoteRepositories=https://nexus.mekomsolutions.net/repository/maven-public -Dartifact=net.mekomsolutions:ozone-distro:$VERSION:zip -Dtransitive=false --legacy-local-repository && \
 ./mvnw org.apache.maven.plugins:maven-dependency-plugin:3.2.0:unpack -Dproject.basedir=$OZONE_DIR -Dartifact=net.mekomsolutions:ozone-distro:$VERSION:zip -DoutputDirectory=$OZONE_DIR/ozone-distro-$VERSION
-
-export DISTRO_PATH=$OZONE_DIR/ozone-distro-$VERSION;  \
-export OPENMRS_CONFIG_PATH=$DISTRO_PATH/openmrs_config;  \
-export OZONE_CONFIG_PATH=$DISTRO_PATH/ozone_config;  \
-export OPENMRS_CORE_PATH=$DISTRO_PATH/openmrs_core;  \
-export OPENMRS_MODULES_PATH=$DISTRO_PATH/openmrs_modules;  \
-export EIP_PATH=$DISTRO_PATH/eip_config; \
-export SPA_PATH=$DISTRO_PATH/spa;\
-export SENAITE_CONFIG_PATH=$DISTRO_PATH/senaite_config; \
-export ODOO_EXTRA_ADDONS=$DISTRO_PATH/odoo_config/addons;\
-export ODOO_CONFIG_PATH=$DISTRO_PATH/odoo_config/odoo_csv;\
-export ODOO_CONFIG_FILE_PATH=$DISTRO_PATH/odoo_config/config/inializer_config.json;\
+# Export required environment variables
+export DISTRO_PATH=$OZONE_DIR/ozone-distro-$VERSION
+export OPENMRS_CONFIG_PATH=$DISTRO_PATH/openmrs_config
+export OZONE_CONFIG_PATH=$DISTRO_PATH/ozone_config
+export OPENMRS_CORE_PATH=$DISTRO_PATH/openmrs_core
+export OPENMRS_MODULES_PATH=$DISTRO_PATH/openmrs_modules
+export EIP_PATH=$DISTRO_PATH/eip_config
+export SPA_PATH=$DISTRO_PATH/spa
+export SENAITE_CONFIG_PATH=$DISTRO_PATH/senaite_config
+export ODOO_EXTRA_ADDONS=$DISTRO_PATH/odoo_config/addons
+export ODOO_CONFIG_PATH=$DISTRO_PATH/odoo_config/odoo_csv
+export ODOO_CONFIG_FILE_PATH=$DISTRO_PATH/odoo_config/config/inializer_config.json
 docker-compose up -d
