32e1cf34cda4df3d4ffae3ab202e51649a6f862b
Emmanuel Nyachoke <achachiez@gmail.com>
2022-11-29 08:51:08 +0300

    OZ-93: Add ability to use docker compose v2 or v1 based on the docker version

--- start-demo.sh
+++ start-demo.sh
@@ -18,4 +18,17 @@ export ODOO_EXTRA_ADDONS=$DISTRO_PATH/odoo_config/addons
 export ODOO_CONFIG_PATH=$DISTRO_PATH/odoo_config/odoo_csv
 export ODOO_INITIALIZER_CONFIG_FILE_PATH=$DISTRO_PATH/odoo_config/config/initializer_config.json
 export ODOO_CONFIG_FILE_PATH=$DISTRO_PATH/odoo_config/config/odoo.conf
-docker-compose up -d --build
+DOCKER_SERVER_VERSION=$(docker version -f "{{.Server.Version}}")
+DOCKER_SERVER_VERSION_MAJOR=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 1)
+DOCKER_SERVER_VERSION_MINOR=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 2)
+DOCKER_SERVER_VERSION_BUILD=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 3)
+
+if [ "${DOCKER_SERVER_VERSION_MAJOR}" -ge 20 ] && \
+   [ "${DOCKER_SERVER_VERSION_MINOR}" -ge 10 ]  && \
+   [ "${DOCKER_SERVER_VERSION_BUILD}" -ge 13 ]; then
+    echo "Docker version >= 20.10.13 using docker compose v2"
+    docker compose up -d --build
+else
+    echo "Docker version < 20.10.13 using docker compose v1"
+    docker-compose up -d --build
+fi
