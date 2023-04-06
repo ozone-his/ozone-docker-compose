53cf0cca98c75ea76cba4a726bc230ca4970fc97
kdaud <dkakumirizii@gmail.com>
2023-04-06 12:21:29 +0300

    Removed unused variables

--- destroy-demo.sh
+++ destroy-demo.sh
@@ -15,19 +15,14 @@ export ODOO_EXTRA_ADDONS=$DISTRO_PATH/odoo_config/addons
 export ODOO_CONFIG_PATH=$DISTRO_PATH/odoo_config/odoo_csv
 export ODOO_CONFIG_FILE_PATH=$DISTRO_PATH/odoo_config/config/inializer_config.json;
 DOCKER_SERVER_VERSION=$(docker version -f "{{.Server.Version}}")
-DOCKER_SERVER_VERSION_MAJOR=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 1)
-DOCKER_SERVER_VERSION_MINOR=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 2)
-DOCKER_SERVER_VERSION_BUILD=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 3)
 
 INSTALLED_DOCKER_VERSION=$(docker version -f "{{.Server.Version}}")
 MINIMUM_REQUIRED_DOCKER_VERSION_REGEX="^((([2-9][1-9]|[3-9][0]|[0-9]{3,}).*)|(20\.([0-9]{3,}|[1-9][1-9]|[2-9][0]).*)|(20\.10\.([0-9]{3,}|[2-9][0-9]|[1][3-9])))"
 if [[ $INSTALLED_DOCKER_VERSION =~ $MINIMUM_REQUIRED_DOCKER_VERSION_REGEX ]]; then
-    echo "Docker version >= 20.10.13, using Docker Compose V2"
     if command -v gp version &> /dev/null; then
         export PROXY_TLS="-DgitPodEnvironment"
     fi
     docker compose down -v
 else
     echo "Docker versions < 20.10.13 are not supported"
-    docker-compose down -v
 fi
