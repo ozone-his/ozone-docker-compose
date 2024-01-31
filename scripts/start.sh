7adf33d04ae281babe7261a2305f337919d8726f
ruhanga <ruhanganathan@gmail.com>
2024-01-31 22:24:49 +0300

    Address review comments

--- scripts/start.sh
+++ scripts/start.sh
@@ -33,9 +33,9 @@ INSTALLED_DOCKER_VERSION=$(docker version -f "{{.Server.Version}}")
 MINIMUM_REQUIRED_DOCKER_VERSION_REGEX="^((([2-9][1-9]|[3-9][0]|[0-9]{3,}).*)|(20\.([0-9]{3,}|[1-9][1-9]|[2-9][0]).*)|(20\.10\.([0-9]{3,}|[2-9][0-9]|[1][3-9])))"
 if [[ $INSTALLED_DOCKER_VERSION =~ $MINIMUM_REQUIRED_DOCKER_VERSION_REGEX ]]; then
     if command -v gp version &> /dev/null; then
-        export SENAITE_GITPOD_HTTPS_REWRITE="true"
+        export GITPOD_ENV="true"
     else
-        export SENAITE_GITPOD_HTTPS_REWRITE="false"
+        export GITPOD_ENV="false"
     fi
 
     # Pull Ozone Docker images
