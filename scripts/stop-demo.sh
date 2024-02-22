e7faa9ce660ed0e5ba4ce2724691f5f5a31ce522
rbuisson <romain@mekomsolutions.com>
2024-02-22 16:57:25 +0100

    OZ-196: Add stop-demo.sh script

---
+++ scripts/stop-demo.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+source utils.sh
+
+# Set CLI options
+setDockerComposeCLIOptions
+
+# Export the DISTRO_PATH variable
+setupDirs
+
+# Export more environment variables
+exportPaths
+
+INSTALLED_DOCKER_VERSION=$(docker version -f "{{.Server.Version}}")
+MINIMUM_REQUIRED_DOCKER_VERSION_REGEX="^((([2-9][1-9]|[3-9][0]|[0-9]{3,}).*)|(20\.([0-9]{3,}|[1-9][1-9]|[2-9][0]).*)|(20\.10\.([0-9]{3,}|[2-9][0-9]|[1][3-9])))"
+if [[ $INSTALLED_DOCKER_VERSION =~ $MINIMUM_REQUIRED_DOCKER_VERSION_REGEX ]]; then
+    echo "$INFO Stopping demo service..."
+    docker compose -p ozone $dockerComposeDemoCLIOptions stop
+    echo "$INFO Stopping proxy service..."
+    docker compose -p ozone $dockerComposeProxyCLIOptions stop
+    echo "$INFO Stopping Ozone services..."
+    docker compose -p ozone $dockerComposeOzoneCLIOptions stop
+else
+    echo "$ERROR Docker versions < 20.10.13 are not supported"
+fi
