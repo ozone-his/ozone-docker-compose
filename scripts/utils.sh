9b618ed3aea6717fc1b3792a23269d1e25052bc0
Kipchumba C. Bett <kbett68@gmail.com>
2024-09-24 14:56:25 +0300

    OZ-708: Add fhir-odoo sidecar service

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -109,10 +109,14 @@ function setTraefikHostnames {
     export ODOO_HOSTNAME=erp-"${IP_WITH_DASHES}.traefik.me"
     export SENAITE_HOSTNAME=lims-"${IP_WITH_DASHES}.traefik.me"
     export ERPNEXT_HOSTNAME=erpnext-"${IP_WITH_DASHES}.traefik.me"
+    export FHIR_ODOO_HOSTNAME=fhir-erp-"${IP_WITH_DASHES}.traefik.me"
+    export ODOO_RPC_PROTOCOL="https"
     echo "→ O3_HOSTNAME=$O3_HOSTNAME"
     echo "→ ODOO_HOSTNAME=$ODOO_HOSTNAME"
     echo "→ SENAITE_HOSTNAME=$SENAITE_HOSTNAME"
     echo "→ ERPNEXT_HOSTNAME=$ERPNEXT_HOSTNAME"
+    echo "→ FHIR_ODOO_HOSTNAME=$FHIR_ODOO_HOSTNAME"
+    echo "→ ODOO_RPC_PROTOCOL=$ODOO_RPC_PROTOCOL"
 
 }
 
@@ -123,10 +127,14 @@ function setNginxHostnames {
     export ODOO_HOSTNAME="localhost:8069"
     export SENAITE_HOSTNAME="localhost:8081"
     export ERPNEXT_HOSTNAME="localhost:8082"
+    export FHIR_ODOO_HOSTNAME="localhost:8083"
+    export ODOO_RPC_PROTOCOL="http"
     echo "→ O3_HOSTNAME=$O3_HOSTNAME"
     echo "→ ODOO_HOSTNAME=$ODOO_HOSTNAME"
     echo "→ SENAITE_HOSTNAME=$SENAITE_HOSTNAME"
     echo "→ ERPNEXT_HOSTNAME=$ERPNEXT_HOSTNAME"
+    echo "→ FHIR_ODOO_HOSTNAME=$FHIR_ODOO_HOSTNAME"
+    echo "→ ODOO_RPC_PROTOCOL=$ODOO_RPC_PROTOCOL"
 
 }
 
