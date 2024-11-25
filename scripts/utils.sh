067a772b9d7307c099d5a4da2d0519927c3284b9
ruhanga <ruhanganathan@gmail.com>
2024-11-25 15:40:45 +0300

    General cleanup

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -38,6 +38,8 @@ function exportPaths () {
     export SQL_SCRIPTS_PATH=$DISTRO_PATH/data/
     export ERPNEXT_CONFIG_PATH=$DISTRO_PATH/configs/erpnext/initializer_config/
     export ERPNEXT_SCRIPTS_PATH=$DISTRO_PATH/binaries/erpnext/scripts/
+    export BAHMNI_OPENMRS_MODULES_PATH=$DISTRO_PATH/binaries/bahmniemr/modules
+
 
     echo "→ OPENMRS_CONFIG_PATH=$OPENMRS_CONFIG_PATH"
     echo "→ OPENMRS_PROPERTIES_PATH=$OPENMRS_PROPERTIES_PATH"
