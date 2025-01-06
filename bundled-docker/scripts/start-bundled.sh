08069db1dc88f8b2bde31c142d0246d51019056b
Kipchumba Bett <kipchubett@gmail.com>
2025-01-06 18:56:38 +0300

    Filter docker compose files correctly

--- bundled-docker/scripts/start-bundled.sh
+++ bundled-docker/scripts/start-bundled.sh
@@ -26,22 +26,26 @@ setupProjectName
 if ! isOzoneRunning "$PROJECT_NAME"; then
     echo "$INFO Starting Ozone project with name: $PROJECT_NAME"
 else
-  echo "$WARN Ozone project with name: $PROJECT_NAME is already running"
-  echo "$INFO Re-applying Docker Compose up command to ensure all services are up-to-date..."
+    echo "$WARN Ozone project with name: $PROJECT_NAME is already running"
+    echo "$INFO Re-applying Docker Compose up command to ensure all services are up-to-date..."
 fi
 
 INSTALLED_DOCKER_VERSION=$(docker version -f "{{.Server.Version}}")
 MINIMUM_REQUIRED_DOCKER_VERSION_REGEX="^((([2-9][1-9]|[3-9][0]|[0-9]{3,}).*)|(20\.([0-9]{3,}|[1-9][1-9]|[2-9][0]).*)|(20\.10\.([0-9]{3,}|[2-9][0-9]|[1][3-9])))"
 if [[ $INSTALLED_DOCKER_VERSION =~ $MINIMUM_REQUIRED_DOCKER_VERSION_REGEX ]]; then
-  # Export scheme
-  exportScheme
-  # Create the networks
-  docker network inspect web >/dev/null 2>&1 ||  docker network create web
-  # Run the docker compose command
-  docker compose -p $PROJECT_NAME --env-file ../.env -f ../docker-compose-bundled.yml -f ../docker-compose-bundled-sso.yml up -d
+    # Export scheme
+    exportScheme
+    # Create the networks
+    docker network inspect web >/dev/null 2>&1 ||  docker network create web
+    # Run the docker compose command
+    if [ "$ENABLE_SSO" == "true" ]; then
+        docker compose -p $PROJECT_NAME --env-file ../.env -f ../docker-compose-bundled.yml -f ../docker-compose-bundled-sso.yml up -d
+    else
+        docker compose -p $PROJECT_NAME --env-file ../.env -f ../docker-compose-bundled.yml up -d
+    fi
 else
-  echo "$ERROR Docker version $INSTALLED_DOCKER_VERSION is not supported. Please install Docker version >= 20.10.0"
-  exit 1
+    echo "$ERROR Docker version $INSTALLED_DOCKER_VERSION is not supported. Please install Docker version >= 20.10.0"
+    exit 1
 fi
 
 # Static list of services
