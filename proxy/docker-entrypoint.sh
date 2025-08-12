c6b5543ed0df0861aba2bae74ff153154f8c6b4a
corneliouzbett <kipchubett@gmail.com>
2025-08-12 10:16:11 +0300

    Ensure entrypoint script is executable

--- proxy/docker-entrypoint.sh
+++ proxy/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 envsubst < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
 mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
-exec nginx -g 'daemon off;'
+exec nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
